1bc11759bf4654c54d4b515eca29fed7cc362e34
Jeremie Dimino <jeremie@dimino.org>
2017-11-13 00:48:07 +0000

    blah

--- bin/forward.ml
+++ bin/forward.ml
@@ -1,24 +1,31 @@
-let add_to_log fn c =
+let add_to_log fn buf n =
   let fd = Unix.openfile fn [O_WRONLY; O_APPEND; O_CREAT] 0o666 in
-  let s =
-    if c = '\n' then
-      "\n\\n\n"
-    else
-      Char.escaped c
-  in
-  let len = String.length s in
-  assert (Unix.write fd s 0 len = len);
+  for i = 0 to n - 1 do
+    let c = Bytes.get buf i in
+    let s =
+      if c = '\n' then
+        "\n\\n\n"
+      else
+        Char.escaped c
+    in
+    let len = String.length s in
+    assert (Unix.write fd s 0 len = len);
+  done;
   Unix.close fd
 
 let forward (fd1, fd2, log) =
-  let buf = Bytes.create 1 in
+  let buf = Bytes.create 1024 in
   let rec loop () =
-    if Unix.read fd1 buf 0 1 = 1 then begin
+    let n = Unix.read fd1 buf 0 (Bytes.length buf) in
+    if n > 0 then begin
       (match log with
        | None -> ()
-       | Some fn -> add_to_log fn buf.[0]);
-      if Unix.write fd2 buf 0 1 = 1 then
-        loop ()
+       | Some fn -> add_to_log fn buf n);
+      let p = ref 0 in
+      while n - !p > 0 do
+        p := !p + Unix.write fd2 buf !p (n - !p)
+      done;
+      loop ()
     end
   in
   loop ()
